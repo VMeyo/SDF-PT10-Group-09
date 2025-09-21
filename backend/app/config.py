ae0f205a74d3ee8b3dae3fec7ea16eeb219c805c
rowneywebster <oumarowney@gmail.com>
2025-09-21 10:11:13 +0300

    Track migrations folder for deployment

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-
-    ##testing
