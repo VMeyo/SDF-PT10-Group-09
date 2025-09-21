c8bc46da194a999c53c1c58d7443cd7200103691
rowneywebster <oumarowney@gmail.com>
2025-09-21 09:28:42 +0300

    database migration

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,11 @@
-# run.py
-from app import create_app
+from app import create_app, db
+from flask_migrate import upgrade
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Run migrations at startup
+with app.app_context():
+    upgrade()
 
+if __name__ == "__main__":
+    app.run()
