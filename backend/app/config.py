a3985bea44257c5910e068b84f0841bb749f04ca
rowneywebster <oumarowney@gmail.com>
2025-09-25 15:01:56 +0300

    added SSL

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///app.db"  # fallback for local development
+        "sqlite:///app.db"  # fallback for local dev
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "superjwtsecret")
@@ -13,7 +13,7 @@ class Config:
     if SQLALCHEMY_DATABASE_URI.startswith("postgresql"):
         SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"sslmode": "require"}}
 
-    # Mail configuration
+    # Mail config
     MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
     MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "True") == "True"
@@ -21,6 +21,7 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+
 
 
 
