46813f11684ed0c04aaf089876de8b8971bcb1b7
rowneywebster <oumarowney@gmail.com>
2025-09-30 00:44:18 +0300

    Updated new url link

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -17,7 +17,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     # Enable CORS for frontend URLs
-    CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5173", "http://localhost:5173", "https://sdf-pt-10-group-09-81kn.vercel.app"
+    CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5173", "http://localhost:5173", "https://sdf-pt-10-group-09.vercel.app/"
 ]}})
 
     # Ensure MAIL_DEFAULT_SENDER is correctly set
